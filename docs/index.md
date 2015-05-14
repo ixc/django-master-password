90b278521a6cbb435fa9055d2f6c7d4c08503eb3
Tai Lee <real.human@mrmachine.net>
2015-05-15 01:52:04 +1000

    Add master password authentication backend and mixin.

--- docs/index.md
+++ docs/index.md
@@ -1,6 +1,15 @@
 # Overview
 
-TODO
+This app provides a mixin class that adds fallback master password
+authentication to an existing backend, and a ready to use subclass of Django's
+`ModelBackend` with master password authentication.
+
+This could be dangerous and is generally not recommended for production, but is
+super handy for development and staging environments.
+
+In a pinch it can also be used temporarily (with a strong password) to
+troubleshoot end-user issues in production environments, without having to
+reset their password.
 
 ## Table of Contents
 
@@ -9,17 +18,68 @@ TODO
 
 ## Installation
 
-Install the app into your virtualenv:
+Just update the `AUTHENTICATION_BACKEND` setting:
 
-    (venv)$ pip install -e git+ssh://git@github.com/ixc/<app_name>.git#egg=<app_name>
+    AUTHENTICATION_BACKEND += ('master_password.auth.ModelBackend', )
 
-Update your settings module:
+If you want to use the optional `make_password` management command, update the
+`INSTALLED_APPS` setting as well:
 
     INSTALLED_APPS += ('master_password', )
 
 ## Usage
 
-TODO
+The `MasterPasswordMixin.authenticate()` method will first try to authenticate
+with its superclass, and then it will fallback to master password
+authentication.
+
+The default implementation authenticates against the `MASTER_PASSWORDS`
+setting, which should be a dictionary with clear text or hashed passwords as
+keys, and callback functions (or `None`) as values.
+
+A callback function must take a user object as its only argument, and should
+return `True` if the user is allowed to authenticate with that password.
+
+For example, you might have one master password that cannot be used for staff
+or superuser accounts, and another that can be used for any account:
+
+    MASTER_PASSWORDS = {
+        'user123': lambda u: not u.is_staff and not u.is_superuser,
+        'superuser123': None,
+    }
+
+The use of clear text master passwords is intended as a convenience during
+development. If you want to enable master password authentication for staging
+or production, you should used a hashed password:
+
+    MASTER_PASSWORDS = {
+        'pbkdf2_sha256$'
+        '20000$'
+        'kGdCcfmJtsUY$'
+        'euTmHbJ9sdHirlsM2MvUjHQPDJ6CZdu02gYrxY3aAbI=': None,
+    }
+
+You can generate a hashed password locally in your development environment:
+
+    >>> from django.contrib.auth.models import make_password
+    >>> print make_password('password123')
+    pbkdf2_sha256$20000$kGdCcfmJtsUY$euTmHbJ9sdHirlsM2MvUjHQPDJ6CZdu02gYrxY3aAbI=
+
+Or use the `make_password` management command as a shortcut:
+
+    (venv)$ ./manage.py make_password
+    Password:
+    Hashed password: pbkdf2_sha256$20000$kGdCcfmJtsUY$euTmHbJ9sdHirlsM2MvUjHQPDJ6CZdu02gYrxY3aAbI=
+
+## Customising
+
+If you are already using a custom auth backend, use the mixin class to add
+master password authentication to it. You will need to define a
+`get_user_object(**kwargs)` method, which should be the same as the
+`authenticate()` method on the superclass but without any password validation.
+
+You can also override the `get_master_passwords()` method if you want to get
+master passwords from another source than the `MASTER_PASSWORDS` setting.
 
 ## HTML Docs
 
--- master_password/tests/tests.py
+++ master_password/tests/tests.py
@@ -4,31 +4,84 @@ Tests for ``master_password`` app.
 
 # WebTest API docs: http://webtest.readthedocs.org/en/latest/api.html
 
-from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.core.management import call_command
+from django.test import override_settings
+from django.test.utils import captured_stdin, captured_stdout
 from django_dynamic_fixture import G
 from django_webtest import WebTest
 
-from master_password import forms, models, views
-from master_password.tests import models as test_models
+from master_password import auth
+
+User = get_user_model()
+
+MASTER_PASSWORDS = {
+    'user123': lambda u: not u.is_staff and not u.is_superuser,
+    make_password('staff123'): lambda u: u.is_staff,
+    'superuser123': None,
+}
 
 
-class Forms(WebTest):
-    def test(self):
-        pass
+@override_settings(MASTER_PASSWORDS=MASTER_PASSWORDS)
+class Auth(WebTest):
+    """
+    Tests for the ``auth`` module.
+    """
+
+    def test_ModelBackend(self):
+        # Generate a random password.
+        password = User.objects.make_random_password()
+
+        # Create a user account.
+        user = G(User)
+        user.set_password(password)
+        user.save()
+
+        # Create a staff account.
+        staff = G(User, is_staff=True)
+        staff.set_password(password)
+        staff.save()
+
+        # Create a superuser account.
+        superuser = G(User, is_superuser=True)
+        superuser.set_password(password)
+        superuser.save()
+
+        # Normal password validation still works.
+        self.assertFalse(
+            self.client.login(username='wrong', password='wrong'))
+        self.assertFalse(
+            self.client.login(username=user.username, password='wrong'))
+        self.assertTrue(
+            self.client.login(username=user.username, password=password))
+
+        # Master passwords can apply to all users, if no callback is defined.
+        self.assertTrue(self.client.login(
+            username=user.username, password='superuser123'))
+        self.assertTrue(self.client.login(
+            username=superuser.username, password='superuser123'))
+
+        # Or just a subset of users.
+        self.assertTrue(
+            self.client.login(username=user.username, password='user123'))
+        self.assertFalse(self.client.login(
+            username=superuser.username, password='user123'))
+
+        # You can store hashed master passwords instead of clear text.
+        self.assertTrue(self.client.login(
+            username=staff.username, password='staff123'))
 
 
-class Models(WebTest):
-    def test_BaseModel(self):
-        """
-        Test that ``modified`` field is updated on save.
-        """
-        obj = G(test_models.BaseModel)
-        modified = obj.modified
-        obj.save()
-        self.assertNotEqual(obj.modified, modified)
+class Management(WebTest):
+    """
+    Tests for management commands.
+    """
 
-
-class Views(WebTest):
-    def test_index(self):
-        response = self.app.get(reverse('master_password_index'))
-        response.mustcontain('Hello World')
+    def test_make_password(self):
+        with captured_stdin() as stdin, captured_stdout() as stdout:
+            stdin.write('password123')
+            stdin.seek(0)
+            call_command('make_password')
+            self.assertIn(
+                'Hashed password: pbkdf2_sha256$20000$', stdout.getvalue())
